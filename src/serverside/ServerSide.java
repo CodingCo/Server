74bf9648c262be704166badc65e07d277dc80d81
Simon Grønborg <groenborg27@gmail.com>
2014-09-09 14:55:16 +0200

    Server Project startup
    	Project created and added.
    	.gitignore file added
    
    	Commit: Simon Grønborg

---
+++ src/serverside/ServerSide.java
@@ -0,0 +1,16 @@
+package serverside;
+
+/**
+ *
+ * @author simon
+ */
+public class ServerSide {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
