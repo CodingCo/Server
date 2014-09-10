4bc03f59a1c4e00a26c327f54f9ffc662a29ddb7
Simon Grønborg <groenborg27@gmail.com>
2014-09-10 19:53:03 +0200

    COMMIT FOR REDESIGN
    	TOTAL REDESIGN OF PROJECT FROM HERE
    	ABSTRACTION NEEDED
    
    	Commit: Simon

--- src/chatserver/ChatServer.java
+++ src/chatserver/ChatServer.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
